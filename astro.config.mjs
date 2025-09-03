7c4ea04a0e7489ce2e8893fae02538754b0175e7
tinnitusi <tinnitusi@ndy.onl>
2025-09-03 10:14:15 +0200

    feat: Integrate Foxi theme structure and reconnect TinaCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://foxi.netlify.app/",
+  integrations: [
+    react(),
+    tailwind(),
+    icon(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
